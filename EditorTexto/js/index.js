0d4538bc88dd647f7cdbb29073130e62797c6c68
marriton <marriton@gmail.com>
2020-03-27 20:03:56 -0300

    Editor de texo

---
+++ EditorTexto/js/index.js
@@ -0,0 +1,11 @@
+function enableTextArea(){
+    //document.getElementsByClassName("textArea").designMode = "On";
+    richTextField.designMode = "On";
+}
+
+function execCmd(comando){
+    richTextField.document.execCommand(comando, false, null);
+}
+
+
+document.getElementById("rodape").innerHTML = Date.toString();
